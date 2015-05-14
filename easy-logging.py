0d33cefa82096ceb6a8d3161376c243611a2770d
Nikos <nikos@habanita.org>
2015-05-14 06:53:22 +0200

    Update easy-logging.py

--- easy-logging.py
+++ easy-logging.py
@@ -1,4 +1,4 @@
-#  ***** GPL LICENSE BLOCK *****
+#  ***** GPL LICENSE BLOCK ***** NOT FUNCTIONNAL BRANCH
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
